b4bd5d759f9ea46cb4ec1056a49add55f6139b66
ShaoAnLin <numbers89757@gmail.com>
2017-03-09 23:53:58 +0800

    Get the event from witch house

--- music_show_crawler.py
+++ music_show_crawler.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -10,6 +11,8 @@ class Show:
         self._name = name
     def set_date(self, date):
         self._date = date
+    def set_time(self, time):
+    	self._time = time
     def set_year(self, year):
         self._year = year
     def set_price(self, price):
@@ -17,9 +20,11 @@ class Show:
     def set_drink(self, drink):
         self._drink = drink
 
+#======================= Functions =========================
+# TODO: show time can only be obtained by clicking "More Info."
 def river_bank_crawler(show_list, url):
 	res = requests.get(url)
-
+	res.encoding = 'utf8'
 	soup = BeautifulSoup(res.text, "html.parser")
 	data = soup.select('.concerts')
 
@@ -44,16 +49,47 @@ def river_bank_crawler(show_list, url):
 		ofile.write('(%d)\nname: %s\ndate: %s\nyear: %s\n' % (show._idx + 1, show._name, show._date, show._year))
 		ofile.write('price: %s\ndrink: %s\n\n' % (show._price, show._drink))
 
+def witch_house_crawler(show_list, url):
+	res = requests.get(url)
+	res.encoding = 'utf8'
+	soup = BeautifulSoup(res.text, "html.parser")
+	data = soup.select('.event-group')
+
+	idx = 0
+	for value in data[0].select('.event-name'):
+		show_list.append( Show(idx, value.text) )
+		idx += 1
+
+	idx = 0
+	for value in data[0].select('.event-title-group'):
+		event_date = '%s/%s(%s)' % (
+			value.select('.date')[0].text, 
+			value.select('.date')[2].text, 
+			value.select('.weekday')[0].text)
+		event_time = '%s-%s' % (value.select('.time')[0].text, value.select('.time')[1].text)
+		show_list[idx].set_date(event_date)
+		show_list[idx].set_time(event_time)
+		idx += 1
+
+	for show in show_list:
+		ofile.write('(%d)\nname: %s\ndate: %s\ntime: %s\n\n' % (show._idx + 1, show._name, show._date, show._time))
+
+#======================= Main =========================
 ofile = open('out.txt', 'w')
 
 river_bank_cafe_url = 'http://www.riverside.com.tw/index.php?option=com_cafe'
 red_house_url = 'http://www.riverside.com.tw/index.php?option=com_livehouse'
+witch_house_url = 'http://www.witchhouse.org/#event'
 
 red_house_list = []
 river_bank_cafe_list = []
+witch_house_list = []
 
 ofile.write('===== red house =====\n')
 river_bank_crawler(red_house_list, red_house_url)
 
 ofile.write('===== river bank cafe =====\n')
 river_bank_crawler(river_bank_cafe_list, river_bank_cafe_url)
+
+ofile.write('===== witch house =====\n')
+witch_house_crawler(witch_house_list, witch_house_url)
